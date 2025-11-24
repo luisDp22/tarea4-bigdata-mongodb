e9e323cbdf15a58c88a27ecfea9bef45e1f089ef
luisDp22 <144968223+luisDp22@users.noreply.github.com>
2025-11-24 10:08:21 -0500

    Update 03_consultas_agregacion.js

--- queries/queries/03_consultas_agregacion.js
+++ queries/queries/03_consultas_agregacion.js
@@ -1,119 +1,73 @@
-// CONSULTAS DE AGREGACIÓN - CINEPLUS
+// CONSULTAS DE AGREGACIÓN - ANÁLISIS ESTADÍSTICO
+// Tarea 4 - Big Data - UNAD
 
-// 1. Promedio de calificaciones por película
+use cineplus
+
+print("=== CONSULTAS DE AGREGACIÓN ===");
+
+// 1. CONTAR - Reseñas por película
+print("1. Top películas más reseñadas:");
 db.reviews.aggregate([
-    {
-        $group: {
-            _id: "$pelicula",
-            promedio_calificacion: { $avg: "$calificacion" },
-            total_resenas: { $sum: 1 }
-        }
-    },
-    {
-        $sort: { promedio_calificacion: -1 }
+  {
+    $group: {
+      _id: "$movie_title",
+      total_reviews: { $sum: 1 }
     }
+  },
+  { $sort: { "total_reviews": -1 } },
+  { $limit: 5 }
 ]);
 
-// 2. Top 5 directores mejor calificados
+// 2. PROMEDIAR - Rating promedio por película
+print("2. Top películas mejor calificadas:");
 db.reviews.aggregate([
-    {
-        $group: {
-            _id: "$director",
-            calificacion_promedio: { $avg: "$calificacion" },
-            total_peliculas: { $addToSet: "$pelicula" }
-        }
-    },
-    {
-        $project: {
-            director: "$_id",
-            calificacion_promedio: 1,
-            cantidad_peliculas: { $size: "$total_peliculas" }
-        }
-    },
-    {
-        $sort: { calificacion_promedio: -1 }
-    },
-    {
-        $limit: 5
+  {
+    $group: {
+      _id: "$movie_title", 
+      average_rating: { $avg: "$rating" },
+      total_reviews: { $sum: 1 }
     }
+  },
+  { $sort: { "average_rating": -1 } },
+  { $limit: 5 }
 ]);
 
-// 3. Distribución de calificaciones por género
+// 3. SUMAR - Votos útiles por película
+print("3. Top películas con más votos útiles:");
 db.reviews.aggregate([
-    {
-        $unwind: "$genero"
-    },
-    {
-        $group: {
-            _id: {
-                genero: "$genero",
-                calificacion: "$calificacion"
-            },
-            cantidad: { $sum: 1 }
-        }
-    },
-    {
-        $sort: { "_id.genero": 1, "_id.calificacion": 1 }
+  {
+    $group: {
+      _id: "$movie_title",
+      total_helpful_votes: { $sum: "$helpful_votes" }
     }
+  },
+  { $sort: { "total_helpful_votes": -1 } },
+  { $limit: 5 }
 ]);
 
-// 4. Reseñas por mes y año
+// 4. CONTAR Y PROMEDIAR - Por género
+print("4. Estadísticas por género:");
 db.reviews.aggregate([
-    {
-        $project: {
-            año: { $substr: ["$fecha", 0, 4] },
-            mes: { $substr: ["$fecha", 5, 2] },
-            pelicula: 1,
-            calificacion: 1
-        }
-    },
-    {
-        $group: {
-            _id: {
-                año: "$año",
-                mes: "$mes"
-            },
-            total_resenas: { $sum: 1 },
-            promedio_calificacion: { $avg: "$calificacion" }
-        }
-    },
-    {
-        $sort: { "_id.año": -1, "_id.mes": -1 }
+  { $unwind: "$genres" },
+  {
+    $group: {
+      _id: "$genres",
+      total_reviews: { $sum: 1 },
+      average_rating: { $avg: "$rating" }
     }
+  },
+  { $sort: { "total_reviews": -1 } }
 ]);
 
-// 5. Usuarios más activos (más reseñas escritas)
+// 5. ESTADÍSTICAS POR IDIOMA
+print("5. Comparación por idioma:");
 db.reviews.aggregate([
-    {
-        $group: {
-            _id: "$usuario",
-            total_resenas: { $sum: 1 },
-            promedio_calificacion: { $avg: "$calificacion" }
-        }
-    },
-    {
-        $sort: { total_resenas: -1 }
-    },
-    {
-        $limit: 10
+  {
+    $group: {
+      _id: "$language",
+      total_reviews: { $sum: 1 },
+      avg_rating: { $avg: "$rating" },
+      total_helpful_votes: { $sum: "$helpful_votes" }
     }
-]);
-
-// 6. Evolución de calificaciones por año de estreno
-db.reviews.aggregate([
-    {
-        $group: {
-            _id: "$ano_estreno",
-            promedio_calificacion: { $avg: "$calificacion" },
-            total_resenas: { $sum: 1 }
-        }
-    },
-    {
-        $match: {
-            _id: { $ne: null }
-        }
-    },
-    {
-        $sort: { _id: 1 }
-    }
+  }
 ]);
